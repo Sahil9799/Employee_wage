f32012f4ba72e22e37434ac7206afe08431fdaea
ADMIN <sahilbambhania@gmail.com>
2022-08-18 17:34:17 +0530

    Add files

---
+++ Rfp_EmpWageCompuAssign/Rfp_EmpWageCompuAssign/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Rfp_EmpWageCompuAssign;
+Console.WriteLine();
+Console.WriteLine("Welcome to Employee Wage Computation Program ");
+AttendanceUc1.Attendance();
+WageUc2.Wage();
+ParttimeUc3.Wage();
+SwitchWageUc4.EmpWage();
+Working20DaysUc5.Emp20DaysWage();
+Max100HrsUc6.EmpMax100HrsWage();
